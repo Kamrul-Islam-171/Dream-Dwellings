7a39285f2699bf6504019622d771bbff1a488dca
Kamrul-Islam-171 <kamrul.ruet.171@gmail.com>
2024-04-12 22:09:59 +0600

    clients feedback done

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,13 +2,13 @@ import { Helmet } from "react-helmet-async";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
-
+import 'animate.css';
 
 
 const About = () => {
     useEffect(() => {
         AOS.init();
-      }, [])
+    }, [])
     return (
         <>
             <Helmet>
@@ -19,7 +19,7 @@ const About = () => {
                 <div className="">
 
                     <div className="grid lg:grid-cols-2 min-h-[calc(100vh-85px)] gap-5 items-center">
-                        <h1 className="md:text-3xl text-xl lg:text-4xl text-center">Welcome to <br /> <span className="lg:text-6xl md:text-5xl text-4xl font-bold">Dream <span className="text-primary-color">Dwellings</span></span></h1>
+                        <h1 className="md:text-3xl text-xl lg:text-4xl text-center animate__animated animate__pulse">Welcome to <br /> <span className="lg:text-6xl md:text-5xl text-4xl font-bold">Dream <span className="text-primary-color">Dwellings</span></span></h1>
                         <p className="lg:text-2xl text-xl text-justify text-gray-600">At Dream Dwellings, we don't just sell houses; we craft dreams into reality. Our story began with a vision to redefine the real estate experience, offering not just properties, but spaces where aspirations thrive and memories are made. Whether you're looking to buy or rent, our diverse portfolio of properties, including single-family houses, apartments, student housing, seaside rental homes, and senior community living options, ensures that there's something for everyone.</p>
                     </div>
 
@@ -28,10 +28,10 @@ const About = () => {
                 <div>
                     <h1 className="text-4xl text-center" data-aos="zoom-in" data-aos-duration="600">Our Story</h1>
                     <div className="grid grid-cols-2 gap-8 w-2/3 mx-auto mt-10">
-                        <div className="bg-secondary-color text-gray-700 px-16 py-16" data-aos='zoom-in-right' data-aos-delay = "300">
+                        <div className="bg-secondary-color text-gray-700 px-16 py-16" data-aos='zoom-in-right' data-aos-delay="300">
                             <p className="text-justify">Founded with a passion for connecting individuals with their ideal living spaces, Dream Dwellings has been transforming houses into homes since 2016.</p>
                         </div>
-                        <div className="bg-third-color text-gray-700 px-16 py-16" data-aos='zoom-in-left' data-aos-delay = "300">
+                        <div className="bg-third-color text-gray-700 px-16 py-16" data-aos='zoom-in-left' data-aos-delay="300">
                             <p className="text-justify">Our journey began with a simple belief: that everyone deserves a place they can truly call their own. Over the years, we've built a reputation for excellence, integrity, and personalized service, earning the trust of countless clients along the way.</p>
                         </div>
                     </div>
@@ -50,31 +50,41 @@ const About = () => {
                     </div>
                 </div>
 
-                <div className="mt-16">
-                    <h1 className="text-4xl text-center">What Sets Us Apart</h1>
-                    <div>
-                        <div>
+                <div className="mt-16 space-y-8">
+                    <h1 className="text-4xl text-center"  data-aos="zoom-in" data-aos-duration = '1000'>What Sets Us Apart</h1>
+                    <div className="grid grid-cols-4 gap-8 ">
+                        <div className="bg-secondary-color p-6 space-y-3 " data-aos="fade-up-left" data-aos-duration = '1000'
+                        data-aos-delay='1000'>
                             <h3 className="text-xl font-medium">Diverse Portfolio</h3>
-                            <p> From spacious single-family houses to modern apartments, cozy student housing, serene seaside rentals, and vibrant senior community living, our diverse portfolio offers something for everyone.</p>
+                            <p className="text-justify"> From spacious single-family houses to modern apartments, cozy student housing, serene seaside rentals, and vibrant senior community living, our diverse portfolio offers something for everyone.</p>
                         </div>
-                        <div>
+                        <div className="bg-third-color p-6 space-y-3 mt-4"
+                        data-aos="fade-up-right" data-aos-duration = '1000' data-aos-delay='1000'>
                             <h3 className="text-xl font-medium">Personalized Approach</h3>
-                            <p> We understand that every individual and family is unique. That's why we take the time to listen to your needs, preferences, and goals, and tailor our services to ensure we find the perfect match for you.</p>
+                            <p className="text-justify"> We understand that every individual and family is unique. That's why we take the time to listen to your needs, preferences, and goals, and tailor our services to ensure we find the perfect match for you.</p>
                         </div>
-                        <div>
+                        <div className="bg-secondary-color p-6 space-y-3 mt-8"
+                        data-aos="fade-up-left" data-aos-duration = '1000'
+                        data-aos-delay='1000'>
                             <h3 className="text-xl font-medium">Expertise</h3>
-                            <p> With years of experience in the real estate industry, our team of professionals brings a wealth of knowledge, insight, and expertise to every transaction.</p>
+                            <p className="text-justify"> With years of experience in the real estate industry, our team of professionals brings a wealth of knowledge, insight, and expertise to every transaction.</p>
                         </div>
-                        <div>
+                        <div className="bg-third-color p-6 space-y-3 mt-12"
+                        data-aos="fade-up-right" data-aos-duration = '1000'
+                        data-aos-delay='1000'>
                             <h3 className="text-xl font-medium">Community Focus</h3>
-                            <p> Beyond just buying or renting properties, we're committed to building thriving communities where residents feel welcomed, supported, and connected.</p>
+                            <p className="text-justify"> Beyond just buying or renting properties, we're committed to building thriving communities where residents feel welcomed, supported, and connected.</p>
                         </div>
                     </div>
                 </div>
 
-                <div className="mt-16">
-                    <h1 className="text-4xl text-center">Our Commitment</h1>
-                    <p>At Dream Dwellings, we understand that finding the right home is a deeply personal and significant decision. That's why we go above and beyond to provide you with the support, guidance, and expertise you need to make the best choice for you and your loved ones. With a focus on integrity, transparency, and exceptional customer service, we strive to make the process of buying or renting a home as smooth and enjoyable as possible.</p>
+                <div className="mt-16 flex flex-row-reverse mb-10 gap-8 h-[500px]">
+                    <div className=" bg-red-300 w-2/5 flex items-center justify-center bg-[url('https://i.ibb.co/T01wvfB/Commitment.jpg')] bg-center bg-cover" data-aos='fade-right' data-aos-duration = '1000' data-aos-delay='2000'>
+                        <h1 className="text-5xl text-center text-white">Our Commitments</h1>
+                    </div>
+                    <div className="w-3/5  flex items-center" data-aos='fade-left' data-aos-duration = '1000' data-aos-delay='2000'>
+                        <p className="text-justify text-xl">At Dream Dwellings, we understand that finding the right home is a deeply personal and significant decision. That's why we go above and beyond to provide you with the support, guidance, and expertise you need to make the best choice for you and your loved ones. With a focus on integrity, transparency, and exceptional customer service, we strive to make the process of buying or renting a home as smooth and enjoyable as possible.</p>
+                    </div>
                 </div>
             </div>
 
--- src/Pages/FeedBack/FeedBack.jsx
+++ src/Pages/FeedBack/FeedBack.jsx
@@ -1,9 +1,163 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 
 const FeedBack = () => {
+    useEffect(() => {
+        AOS.init();
+    }, [])
     return (
-        <div>
-            <h1 className="text-4xl">Our Feedbacks</h1>
+        <div className="container mx-auto mt-36 mb-20">
+            <div>
+                <h1 className="text-4xl text-center mb-16" data-aos="zoom-out" data-aos-duration='1000'>Clients Testimonials</h1>
+
+                <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-8">
+                    <div className="space-y-5 shadow-lg px-10 py-8" data-aos="zoom-in-left"
+                    data-aos-duration='1000'
+                        data-aos-offset="300"
+                        data-aos-easing="ease-in-sine" data-aos-delay='1000'>
+                        <div className="flex gap-8 items-center">
+                            <div className="w-[90px] h-[90px] ">
+                                <img className="w-full h-full rounded-full" src={'https://i.ibb.co/60KZFxw/sarah-m-london-ceo-centene.jpg'} alt="" />
+                            </div>
+                            <div>
+                                <h3 className="text-xl font-medium">Sarah M.</h3>
+                                <p className="text-gray-500">Happy buyer</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p className="text-gray-500">I can't thank Dream Dwellings enough for helping me find my perfect home. Their attention to detail and commitment to finding exactly what I wanted made the whole process a breeze. I highly recommend their services to anyone in search of their dream home.</p>
+                        </div>
+
+                        <div className="rating">
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                        </div>
+
+                    </div>
+
+                    <div className="space-y-5 shadow-lg px-10 py-8" data-aos="zoom-in"
+                    data-aos-duration='1000'
+                        data-aos-offset="300"
+                        data-aos-easing="ease-in-sine" data-aos-delay='1000'>
+                        <div className="flex gap-8 items-center" data-aos="zoom-in-left"
+                        data-aos-offset="300"
+                        data-aos-easing="ease-in-sine" data-aos-delay='1000'>
+                            <div className="w-[90px] h-[90px] ">
+                                <img className="w-full h-full rounded-full" src={'https://i.ibb.co/jb2FTg1/headshot-portrait.jpg'} alt="" />
+                            </div>
+                            <div>
+                                <h3 className="text-xl font-medium">Michael L.</h3>
+                                <p className="text-gray-500">Happy buyer</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p className="text-gray-500">From the initial consultation to the day I moved into my new home, Dream Dwellings was there every step of the way. Their team went above and beyond to ensure I found a place that not only met but exceeded my expectations. I couldn't be happier with my experience.</p>
+                        </div>
+
+                        <div className="rating">
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                        </div>
+
+                    </div>
+
+
+                    <div className="space-y-5 shadow-lg px-10 py-8" data-aos="zoom-in-down"
+                    data-aos-duration='1000'
+                        data-aos-offset="300"
+                        data-aos-easing="ease-in-sine" data-aos-delay='1000'>
+                        <div className="flex gap-8 items-center">
+                            <div className="w-[90px] h-[90px] ">
+                                <img className="w-full h-full rounded-full" src={'https://i.ibb.co/VBrdGsv/emily-read-0.jpg'} alt="" />
+                            </div>
+                            <div>
+                                <h3 className="text-xl font-medium">Emily K.</h3>
+                                <p className="text-gray-500">Happy buyer</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p className="text-gray-500">Dream Dwellings made the daunting task of buying a home feel effortless. Their expertise and professionalism gave me confidence throughout the entire process. I'm grateful for their guidance and would recommend them to anyone looking to find their dream home.</p>
+                        </div>
+
+                        <div className="rating">
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                        </div>
+
+                    </div>
+
+
+                    <div className="space-y-5 shadow-lg px-10 py-8" data-aos="zoom-in-down"
+                    data-aos-duration='1000'
+                        data-aos-offset="300"
+                        data-aos-easing="ease-in-sine" >
+                        <div className="flex gap-8 items-center">
+                            <div className="w-[90px] h-[90px] ">
+                                <img className="w-full h-full rounded-full" src={'https://i.ibb.co/J7508QD/dr-jason-pomerantz-md-106-320x320-2x.jpg'} alt="" />
+                            </div>
+                            <div>
+                                <h3 className="text-xl font-medium"> Jason H.</h3>
+                                <p className="text-gray-500">Happy buyer</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p className="text-gray-500">As a first-time homebuyer, I had a lot of questions and concerns. Dream Dwellings patiently answered all of them and helped me navigate the complexities of the real estate market. Thanks to their expertise, I am now happily settled into my new home. I couldn't have done it without them.</p>
+                        </div>
+
+                        <div className="rating">
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                        </div>
+
+                    </div>
+
+                    <div className="space-y-5 shadow-lg px-10 py-8" data-aos="zoom-in-up"
+                        data-aos-offset="300"
+                        data-aos-duration='1000'
+                        data-aos-easing="ease-in-sine" >
+                        <div className="flex gap-8 items-center">
+                            <div className="w-[90px] h-[90px] ">
+                                <img className="w-full h-full rounded-full" src={'https://i.ibb.co/cc9pyn8/1524080699809-e-2147483647-v-beta-t-PTOhe-4c-HE5-H-w5caxnz7i-Kxafr-MS548-Pd-We-2pkr-DU.jpg'} alt="" />
+                            </div>
+                            <div>
+                                <h3 className="text-xl font-medium">Jessica S.</h3>
+                                <p className="text-gray-500">Happy buyer</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p className="text-gray-500">I had a specific vision for my dream home, and Dream Dwellings made it a reality. They listened to my needs and preferences and worked tirelessly to find properties that aligned with my vision. Thanks to their dedication, I am now living in the home of my dreams.</p>
+                        </div>
+
+                        <div className="rating">
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                        </div>
+
+                    </div>
+
+
+
+
+
+                </div>
+            </div>
         </div>
     );
 };
